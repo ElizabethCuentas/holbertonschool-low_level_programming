65d28dbef9b1c94ab5c6ac224429d1c5b3f3d9c6
ElizabethCuentas <eliiz.cris21@gmail.com>
2021-02-04 23:02:46 -0500

    Fine

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success) Eliz
+ */
+int main(void)
+{
+char l;
+for (l = 'a' ; l <= 'z' ; l++)
+putchar (l);
+for (l = 'A' ; l <= 'Z' ; l++)
+putchar (l);
+putchar ('\n');
+return (0);
+}
