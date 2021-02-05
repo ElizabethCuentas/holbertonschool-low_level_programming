8ccbb0f88da858febef9f6a7c6fc5c365e656aad
ElizabethCuentas <eliiz.cris21@gmail.com>
2021-02-04 23:31:24 -0500

    Base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char l, n;
+for (n = '0'; n <= '9'; n++)
+putchar(n);
+for (l = 'a'; l <= 'f'; l++)
+putchar(l);
+putchar('\n');
+return (0);
+}
