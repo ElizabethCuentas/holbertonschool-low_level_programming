1c9eec0b988306c0e0de9fa8bda97bd050caf03a
ElizabethCuentas <eliiz.cris21@gmail.com>
2021-02-16 22:04:50 -0500

    File new

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,8 +12,11 @@ void print_array(int *a, int n)
 int index;
 
 for (index = 0; index < n; index++)
+{
 printf("%d", a[index]);
-if (index != (n - 1))
+if (index != (n - 1)){
 printf(", ");
+}
 putchar('\n');
+}
 }
