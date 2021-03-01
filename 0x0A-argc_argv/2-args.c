04216edac65c99237c04a1adf0f5668f987ebda8
ElizabethCuentas <eliiz.cris21@gmail.com>
2021-02-28 22:04:24 -0500

    Files modified

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,17 +1,18 @@
-#include "holberton.h"
 #include <stdio.h>
+
 /**
- * main - print all args
- * Return: 0
- * @argc: amount of args
- * @argv: pointer to pointers array
+ * main - Prints all arguments it receives.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
-	while (argc--)
-	{
-		printf("%s\n", *argv++);
-	}
+	int arg;
+
+	for (arg = 0; arg < argc; arg++)
+		printf("%s\n", argv[arg]);
 
 	return (0);
 }
