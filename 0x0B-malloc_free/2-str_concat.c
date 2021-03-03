4c24f3deb1e2215e52b24ae691d3f1b785514e68
ElizabethCuentas <eliiz.cris21@gmail.com>
2021-03-02 21:41:53 -0500

    Files modified =D

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,52 +1,38 @@
 #include "holberton.h"
-#include <stdio.h>
 #include <stdlib.h>
-
 /**
- * str_concat - function that concatenates two strings.
- *              if NULL is passed, treat it as an empty string
- *              The function should return NULL on failure
- *
- * @s1: This is the output string
- * @s2: This is the input string
- *
- * Return: The returned pointer should point to a newly allocated space in
- *         memory which contains the contents of s1, followed by the contents 
- *         of s2, and null terminated
+ * str_concat - concatenates two strings
+ * @s1: string1
+ * @s2: string2
+ * Return: pointer to new string
  */
-
 char *str_concat(char *s1, char *s2)
 {
-	int a, b;
 	char *s;
 
-	if (s1 == NULL)
-	{
-		s1 = "";
-	}
-	if (s2 == NULL)
-	{
-		s2 = "";
-	}
-	for (a = 0; s1[a] != '\0'; a++)
-	{
-	}
-	for (b = 0; s2[b] != '\0'; b++)
-	{
-	}
+	int size1 = 0, size2 = 0, i;
 
-	s = (char *)  malloc(((a + b) + 1) * sizeof(char));
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[size1])
+		size1++;
+	while (s2[size2])
+		size2++;
+
+	s = malloc(sizeof(char) * (size1 + size2 + 1));
 
 	if (s == NULL)
 		return (NULL);
-	for (a = 0; s1[a] != '\0'; a++)
-	{
-		s[a] = s1[a];
-	}
-	for (b = 0; s2[b] != '\0'; b++)
-	{
-		s[a] = s2[b];
-		a++;
-	}
+
+	for (i = 0; i < size1; i++)
+		s[i] = s1[i];
+	for (i = 0; i < size2; i++)
+		s[size1 + i] = s2[i];
+
+	s[size1 + size2] = 0;
 	return (s);
 }
--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,54 +1,41 @@
 #include "holberton.h"
-#include <stdio.h>
 #include <stdlib.h>
-
 /**
- * **alloc_grid - function that returns a pointer to a 2 dimensional array of
- *                integers.
- *
- * @width: This is the columns of the array
- * @height: This is the rows of the array
- *
- * Return: Returns a pointer to a 2 dimensional array of integers.
- *         The function should return NULL on failure
- *         If width or height is 0 or negative, return NULL
+ * alloc_grid - create 2d array
+ * @width: width of the ar
+ * @height: height of the ar
+ * Return: pointer to 2d ar
  */
-
-int **alloc_grid(int width, int height) /* 6 columns and 4 rows*/
+int **alloc_grid(int width, int height)
 {
-	int j, i;
-	int **arr;
+	int **p;
 
-	if (width < 1 || height < 1)
+	int i, j;
+
+	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	arr = (int **)malloc(sizeof(int *) * height);
-	if (arr == NULL)
-	{
-		free(arr);
+	p = malloc(sizeof(int *) * height);
+
+	if (p == NULL)
 		return (NULL);
-	}
 
 	for (i = 0; i < height; i++)
 	{
-		arr[i] = malloc(sizeof(int) * width);
-		if (arr[i] == NULL)
+		*(p + i) = malloc(sizeof(int) * width);
+		if (*(p + i) == NULL)
 		{
-			for (i = 0; i < height; i++)
-			{
-				free(arr[i]);
-			}
-			free(arr);
+			int k = 0;
+
+			for (k = 0; k < i; k++)
+				free(*(p + k));
+			free(p);
 			return (NULL);
 		}
 	}
 
-
 	for (i = 0; i < height; i++)
 		for (j = 0; j < width; j++)
-		{
-			arr[i][j] = 0;
-		}
-
-	return (arr);
+			*(*(p + i) + j) = 0;
+	return (p);
 }
