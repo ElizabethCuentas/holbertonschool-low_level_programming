f6244d548088c4196e5deda8814b24f9cccc8908
ElizabethCuentas <eliiz.cris21@gmail.com>
2021-03-11 21:33:06 -0500

    File new

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,37 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings -  function that prints strings, followed by a new line
+ *
+ * @separator: This is the input separator
+ * @n: This is the number of items
+ *
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int count;
+	char *s;
+
+	if (n <= 0)
+	{
+		printf("\n");
+		return;
+	}
+	va_start(ap, n);
+	for (count = 0; count < n; count++)
+	{
+		s = va_arg(ap, char *);
+		if (s == NULL)
+			printf("(nil)");
+		else
+			printf("%s", s);
+		if (separator != NULL && (count + 1) != n)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(ap);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,11 @@
+# 0x0B. C - malloc, free
+In this directory you will learn how to use malloc and free
+
+
+## Files:
+
+1.  **0-sum_them_all.c**
+2.  **1-print_numbers.c**
+3.  **2-print_strings.c**
+4.  **3-print_all.c**
+
